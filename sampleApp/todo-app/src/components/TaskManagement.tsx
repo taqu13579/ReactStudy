aad29b0fe1e777b0b34f9b2ac9106d63aa740e3c
taqu13579 <92580230+taqu13579@users.noreply.github.com>
2022-05-15 21:30:27 +0900

    Implements function of each function

--- sampleApp/todo-app/src/components/TaskManagement.tsx
+++ sampleApp/todo-app/src/components/TaskManagement.tsx
@@ -15,3 +15,112 @@ import {
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
+const useStyle = makeStyles((theme) => ({
+    taskTime: {
+        fontSize: '8px'
+    }
+}));
+
+type Task = {
+    docId: string;
+    taskText: string;
+    timeStamp: string;
+};
+
+function TaskManagement() {
+    const classes = useStyle();
+    const [taskList, setTaskList] = useState<Task[]>([]);
+    const [taskText, setTaskText] = useState<string>('');
+    const [isOpenDeleteConfirm, setIsOpenDeleteConfirm] = useState(false);
+    const [deleteDocId, setDeleteDocId] = useState<string>('');
+    
+    const dispData = () => {
+        const taskCollectionRef = collection(db, 'tTasks');
+        getDocs(taskCollectionRef).then((querySnapshot) => {
+            const userList: Task[] = [];
+            let count: number = 0;
+            querySnapshot.docs.map((doc, index) => {
+                const task: Task = {
+                    docId: doc.id,
+                    taskText: doc.data().taskText,
+                    timeStamp: doc.data({serverTimestamps: "estimate"}).timeStamp,
+                };
+                userList.push(task);
+                count += 1;
+            });
+            setTaskList(userList);
+        });
+    };
+
+    const addTask = (inputText: string) => {
+        if (inputText === '') {
+            return;
+        }
+        const tasksCollectinRef = collection(db, 'tTasks');
+        const nowTime = new Date();
+        const nowYear = nowTime.getFullYear();
+        const nowMonth = nowTime.getMonth();
+        const nowDay = nowTime.getDay();
+        const nowHour = nowTime.getHours();
+        const nowMin = nowTime.getMinutes();
+        const nowSec = nowTime.getSeconds();
+        const documentRef = addDoc(tasksCollectinRef, {
+            taskText: inputText,
+            timeStamp: `${nowYear}/${nowMonth}/${nowDay} ${nowHour}:${nowMin}:${nowSec}`,
+        });
+        setTaskText('');
+        dispData();
+    };
+
+    const deleteTaskConfirm = (docId: string) => {
+        setDeleteDocId(docId);
+        setIsOpenDeleteConfirm(true);
+    };
+
+    const deleteTask = async() => {
+        setIsOpenDeleteConfirm(false);
+        const userDocumentRef = doc(db, 'tTasks', deleteDocId);
+        await deleteDoc(userDocumentRef);
+        dispData();
+    };
+
+    const changeTaskChecked = (blnChecked: boolean, numIndex: number) => {
+        if (blnChecked === true) {
+            const taskText = document.getElementById(`taskText${numIndex}`);
+            if (taskText !== null) {
+                taskText.style.color = '#FF0000';
+            };
+        } else {
+            const taskText = document.getElementById(`taskText${numIndex}`);
+            if (taskText !== null) {
+                taskText.style.color = '#000000';
+            };
+        };
+    };
+
+    useEffect(() => {
+        dispData();
+    }, []);
+
+    return (
+        <>
+            <TableContainer>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>
+                            </TableCell>
+                            <TableCell>
+                            </TableCell>
+                            <TableCell>
+                            </TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </>
+    )
+}
