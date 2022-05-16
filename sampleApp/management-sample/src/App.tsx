4a29fc867740a5f7b1bc7da238206018b6342485
taqu13579 <92580230+taqu13579@users.noreply.github.com>
2022-05-16 22:24:10 +0900

    management-sample initial page creation

--- sampleApp/management-sample/src/App.tsx
+++ sampleApp/management-sample/src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter , Routes, Route } from 'react-router-dom';
+import ProductPage from './components/pages/ProductPage';
+import HomePage from './components/pages/HomePage';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/products" element={<ProductPage />} />
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ sampleApp/management-sample/src/components/pages/ProductPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ProductPage: React.FC = () => {
+    return (
+        <>商品ページ</>
+    );
+};
+
+export default ProductPage;
